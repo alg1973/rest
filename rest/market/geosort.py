a8fd908b32e4cba2bd621fd6934d4f19cadeb791
Alexey Gadzhiev <alg@ya.ru>
2016-12-02 01:04:51 +0300

    S2 sorting support

---
+++ rest/market/geosort.py
@@ -0,0 +1,32 @@
+import sys
+import s2
+
+#from .models import Restaraunt
+
+
+DEG_OF_100_M=0.00089944
+BASE_LEVEL=14
+CELLS_SIZE=20
+
+#Latitude: 1 deg = 110.54 km, 1 minute (0.01666666666666666666 deg) is 1.853 km
+#so we will use some approximation:
+# (100 m = 0.00090465 or 0.00089944234574563700) 
+#Longitude: 1 deg = 111.320*cos(latitude) km
+
+def get_cells_of_circle(lat,lng):
+#   point = s2.S2LatLng.FromDegrees(lat,lng).ToPoint()
+    cap = s2.S2Cap.FromAxisAngle(s2.S2LatLng.FromDegrees(lat,lng).ToPoint(),
+                                 s2.S1Angle.Degrees(DEG_OF_100_M*3))
+    coverer = s2.S2RegionCoverer()
+    coverer.set_min_level(BASE_LEVEL)
+    coverer.set_max_level(BASE_LEVEL)
+    coverer.set_max_cells(CELLS_SIZE)
+    return  coverer.GetCovering(cap) 
+
+
+for f in  get_cells_of_circle(55.772516, 37.474153):
+    print (f.id()," ")
+
+print ("\n")
+    
+    
