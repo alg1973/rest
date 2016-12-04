a31af714de1165624645beed1eb13b975e95d53e
Alexey Gadzhiev <alg1973@gmail.com>
2016-12-04 18:07:35 +0300

    Try to use rect instead of cap due to SWIG issues

--- rest/market/geosort.py
+++ rest/market/geosort.py
@@ -14,9 +14,14 @@ CELLS_SIZE=20
 #Longitude: 1 deg = 111.320*cos(latitude) km
 
 def get_cells_of_circle(lat,lng):
-#   point = s2.S2LatLng.FromDegrees(lat,lng).ToPoint()
-    cap = s2.S2Cap.FromAxisAngle(s2.S2LatLng.FromDegrees(lat,lng).ToPoint(),
-                                 s2.S1Angle.Degrees(DEG_OF_100_M*3))
+#
+#    cap = s2.S2Cap.FromAxisAngle(s2.S2LatLng.FromDegrees(lat,lng).ToPoint(),
+#                                 s2.S1Angle.Degrees(DEG_OF_100_M*3))
+# There is a problem with SWIG with S2Point class so we use latlngrect instead
+#
+    rect = s2.S2LatLngRect.FromPoint(s2.S2LatLng.FromDegrees(lat,lng))
+    rect=rect.ConvolveWithCap(s2.S1Angle.Degrees(DEG_OF_100_M*3))
+
     coverer = s2.S2RegionCoverer()
     coverer.set_min_level(BASE_LEVEL)
     coverer.set_max_level(BASE_LEVEL)
