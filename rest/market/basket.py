31a44c955bc8af7a518320a25543294a5dc85e04
Alexey Gadzhiev <alg@ya.ru>
2016-12-16 17:03:01 +0300

    quantity update done

--- rest/market/basket.py
+++ rest/market/basket.py
@@ -35,14 +35,19 @@ class Bsk:
             if request.GET.get('q',0)!=0:
                 self.load()
                 meal = Dish.objects.get(pk=request.GET['dish_id'])
-                if self.empty() or (self.restaraunt_id == meal.restaraunt.id): 
-                    basket_ent = BasketEntry(dish=meal,basket=self.basket)
-                    basket_ent.quantity=request.GET['q']
+                if self.empty() or (self.restaraunt_id == meal.restaraunt.id):
+                    try:
+                        basket_ent = BasketEntry.objects.get(dish=meal,basket=self.basket)
+                        basket_ent.quantity+=int(request.GET['q'])
+                    except BasketEntry.DoesNotExist:
+                        basket_ent = BasketEntry(dish=meal,basket=self.basket)
+                        basket_ent.quantity=request.GET['q']
+
                     basket_ent.change_date=timezone.now()
-                    #self.basket.dishes.add(basket_ent)
                     basket_ent.save()
-                    self.basket.save()
-                else:
+                   
+                        
+            else:
                     self.auth.message("Cant add meal to basket from other restaraunt")
 
 
